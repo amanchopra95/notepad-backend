0b34bda014445bf3ec689466848874365a61bb54
Aman Chopra <amanchopra954@gmail.com>
2020-10-04 17:43:33 +0530

    restructuring the app

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+// Routes
+const auth = require('./routes/auth')
+const notes = require('./routes/notes')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/notes', notes)
+
+const server = app.listen(PORT, () => {
+    console.log(`Server has started at http://localhost:${PORT}`)
+})
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error ${err.message}`)
+    server.close(() => process.exit(1))
+})
